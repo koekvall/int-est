93e4ff3e9a63575c7f44773e65dcccf58eb7384d
koekvall <k.o.ekvall@gmail.com>
2021-01-13 08:58:56 +0100

    New figure script

--- figures.R
+++ figures.R
@@ -36,12 +36,10 @@ dat2 <-  t(sapply(lam_vals, function(x)get_statistics(y2, x)))
 
 # Create figure
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-if(PDF) pdf(paste(out_dir, "fig_1.pdf", sep = ""), width = 6, height = 4)
 
+if(PDF) pdf(paste(out_dir, "fig_1.pdf", sep = ""), width = 6, height = 4)
 par(cex.axis = 1.1, cex.lab = 1.1, cex = 1.1)
 par(mgp=c(2.2,0.45,0), tcl=-0.4, mar=c(3.3,3.6,1.5,1.1))
-# par(bty="n")
-#par(mfrow = c(1, 3))
 
 # Likelihood
 plot(lam_vals, dat1[, "ll"], type = "l", lwd = 2,
@@ -53,78 +51,121 @@ lines(lam_vals, dat2[, "ll"], type = "l", lwd = 2, lty = 1,
 
 if(PDF) dev.off()
 
-### Simulations ####
+### Simulation Figures ####
 sim_data <- readRDS("~/GitHub/int-est/sims/sims_big.Rds")
+n_sims <- nrow(sim_data[[1]])
 # Verify this using names(sim_data)
 gamma <- c(0, 0.01, 0.05, seq(0.1, 0.5, length.out = 5))
 n_vec <- c(20, 40, 80)
 
 # Cover curves
-cover <- matrix(0, nrow = length(sim_data), ncol = 5)
+cover <- matrix(0, nrow = length(sim_data), ncol = 8)
 for(ii in 1:nrow(cover)){
-  cover[ii, 1:3] <- 1 - colMeans(sim_data[[ii]][, c("p_val", "lrt_p_val", "wald_p_val")] < 0.05)
+  cover[ii, 1:3] <- 1 - colMeans(sim_data[[ii]][, c("p_val", "lrt_p_val",
+                                                    "wald_p_val")] < 0.05)
+  cover[ii, 4:6] <- apply(sim_data[[ii]][, c("p_val", "lrt_p_val",
+                                             "wald_p_val")] < 0.05,
+                          2, sd) / sqrt(n_sims)
 }
-cover[, 4] <- rep(n_vec, each = length(gamma))
-cover[, 5] <- rep(gamma, length(n_vec))
-colnames(cover) <- c("our", "lrt", "wald", "n", "gamma")
+cover[, 7] <- rep(n_vec, each = length(gamma))
+cover[, 8] <- rep(gamma, length(n_vec))
+colnames(cover) <- c("our", "lrt", "wald", "se_our", "se_lrt", "se_wald",
+                     "n", "gamma")
 
+if(PDF) pdf(paste(out_dir, "fig_2.pdf", sep = ""), width = 10, height = 4)
+par(cex.axis = 1.1, cex.lab = 1.1, cex = 1.1)
+par(mgp=c(2.2,0.45,0), tcl=-0.4, mar=c(3.3,3.6,1.5,1.1))
 par(mfrow = c(1, 2))
-# n = 20
-plot(x = gamma,
-     y = cover[cover[, "n"] == 20, "our"],
-     type = "l",
-     ylim = c(0.85, 1),
-     lwd = 2,
-     ylab = "estimated cover probability",
-     xlab = "scaling",
-     main = "n = 20")
-abline(h = 0.95, lwd = 1)
-lines(x = gamma,
-      y = cover[cover[, "n"] == 20, "lrt"],
-      lty = 2,
-      lwd = 2)
-lines(x = gamma,
-      y = cover[cover[, "n"] == 20, "wald"],
-      lty = 3,
-      lwd = 2)
-# n = 80
-plot(x = gamma,
-     y = cover[cover[, "n"] == 80, "our"],
-     type = "l",
-     ylim = c(0.85, 1),
-     lwd = 2,
-     ylab = "estimated cover probability",
-     xlab = "scaling",
-     main = "n = 80")
-abline(h = 0.95, lwd = 1)
-lines(x = gamma,
-      y = cover[cover[, "n"] == 80, "lrt"],
-      lty = 2,
-      lwd = 2)
-lines(x = gamma,
-      y = cover[cover[, "n"] == 80, "wald"],
-      lty = 3,
-      lwd = 2)
 
-
+for(jj in c(20, 80)){
+  plot_dat <- cover[cover[, "n"] == jj, ]
+  plot(x = gamma,
+       y = plot_dat[, "our"],
+       type = "l",
+       ylim = c(0.82, 1),
+       lwd = 2,
+       ylab = "estimated cover probability",
+       xlab = expression(lambda),
+       main = paste0("n = ", jj),
+       col = cbbPalette[1])
+  arrows(gamma,
+         plot_dat[, "our"],
+         gamma,
+         plot_dat[, "our"] + 2 * plot_dat[, "se_our"],
+         length = 0.05,
+         angle = 90,
+         col = cbbPalette[1])
+  arrows(gamma,
+         plot_dat[, "our"],
+         gamma,
+         plot_dat[, "our"] - 2 * plot_dat[, "se_our"],
+         length = 0.05,
+         angle = 90,
+         col = cbbPalette[1])
+  abline(h = 0.95, lwd = 1)
+  lines(x = gamma,
+        y = plot_dat[, "lrt"],
+        lty = 2,
+        lwd = 2,
+        col = cbbPalette[2])
+  arrows(gamma,
+         plot_dat[, "lrt"],
+         gamma,
+         plot_dat[, "lrt"] + 2 * plot_dat[, "se_lrt"],
+         length = 0.05,
+         angle = 90,
+         col = cbbPalette[2])
+  arrows(gamma,
+         plot_dat[, "lrt"],
+         gamma,
+         plot_dat[, "lrt"] - 2 * plot_dat[, "se_lrt"],
+         length = 0.05,
+         angle = 90,
+         col = cbbPalette[2])
+  lines(x = gamma,
+        y = plot_dat[, "wald"],
+        lty = 3,
+        lwd = 2,
+        col = cbbPalette[3])
+  arrows(gamma,
+         plot_dat[, "wald"],
+         gamma,
+         plot_dat[, "wald"] + 2 * plot_dat[, "se_wald"],
+         length = 0.05,
+         angle = 90,
+         col = cbbPalette[3])
+  arrows(gamma,
+         plot_dat[, "wald"],
+         gamma,
+         plot_dat[, "wald"] - 2 * plot_dat[, "se_wald"],
+         length = 0.05,
+         angle = 90,
+         col = cbbPalette[3])
+}
+if(PDF) dev.off()
 # Distribution
-n_sims <- nrow(sim_data[[1]])
 pp <- ppoints(n_sims)
-
+if(PDF) pdf(paste(out_dir, "fig_3.pdf", sep = ""), width = 10, height = 4)
+par(cex.axis = 1.1, cex.lab = 1.1, cex = 1.1)
+par(mgp=c(2.2,0.45,0), tcl=-0.4, mar=c(3.3,3.6,1.5,1.1))
 par(mfrow = c(1, 2))
+
 # Small but non-zero scale parameters
 plot_dat <- sim_data[[18]]
 plot(x = qchisq(pp, df = 3),
      y = quantile(plot_dat[, "chi_sq"], pp),
      xlab = "theoretical quantiles",
-     ylab = "empirical quantiles",
-     main = "small scale parameters")
+     ylab = "sample quantiles",
+     main = "small scale parameters",
+     col = cbbPalette[1])
 points(x = qchisq(pp, df = 3),
        y = quantile(plot_dat[, "lrt_chi_sq"], pp),
-       pch = 2)
+       pch = 2,
+       col = cbbPalette[2])
 points(x = qchisq(pp, df = 3),
        y = quantile(plot_dat[, "wald_chi_sq"], pp),
-       pch = 3)
+       pch = 3,
+       col = cbbPalette[3])
 abline(a = 0, b = 1, col = "red")
 
 # Large scale parameters
@@ -132,13 +173,16 @@ plot_dat <- sim_data[[24]]
 plot(x = qchisq(pp, df = 3),
      y = quantile(plot_dat[, "chi_sq"], pp),
      xlab = "theoretical quantiles",
-     ylab = "empirical quantiles",
-     main = "large scale parameters")
+     ylab = "sample quantiles",
+     main = "large scale parameters",
+     col = cbbPalette[1])
 points(x = qchisq(pp, df = 3),
        y = quantile(plot_dat[, "lrt_chi_sq"], pp),
-       pch = 2)
+       pch = 2,
+       col = cbbPalette[2])
 points(x = qchisq(pp, df = 3),
        y = quantile(plot_dat[, "wald_chi_sq"], pp),
-       pch = 3)
+       pch = 3,
+       col = cbbPalette[3])
 abline(a = 0, b = 1, col = "red")
-
+if(PDF) dev.off()
